49f1bb070d96d9c5e84cef6d156e802bff76562f
Filip Skoczeń <philipskoczen@gmail.com>
2024-11-13 16:13:41 +0100

    Add plain-article.txt with reading function in index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import * as fs from 'node:fs/promises';
+
+async function readPlainArticle() {
+	let content: string | undefined = undefined;
+	try {
+		content = await fs.readFile('./plain-article.txt', 'utf-8');
+	} catch (e: any) {
+		// "No such file or directory" can be better described by error below
+		if (e?.message != 'No such file or directory') throw e;
+	}
+	if (!content) throw new Error('Please add plain-article.txt file with article content to project root');
+	return content;
+}
+
+console.log(await readPlainArticle())
